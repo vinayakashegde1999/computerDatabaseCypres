3f04153e90e9acd8a18ee2252e476374ca9da137
vinayakashegde <vinayakashegde@gmail.com>
2020-03-09 03:07:45 +0530

    adding first files for cypress with computer database

---
+++ cypress/integration/computerDatabaseUITest/getComputerTest_spec.js
@@ -0,0 +1,16 @@
+let databaseName="TestDatabase"+Math.floor(Date.now() / 1000)
+
+describe('Get newly added  Comupter to databse',function(){
+    it('Successfully add the coputer and retrun to main page',function(){
+        cy.visit('http://computer-database.herokuapp.com/computers')
+        cy.get('.btn.success').click()
+        cy.get("#name").click().type(databaseName)
+        cy.get("#introduced").click().type("2010-01-01")
+        cy.get("#discontinued").click().type("2010-01-01")
+        cy.get("#company").select("Thinking Machines")
+        cy.get('.btn.primary').click()
+        cy.get(".alert-message.warning").should('include.text',`Done! Computer ${databaseName} has been created`)
+           
+    })
+
+});
